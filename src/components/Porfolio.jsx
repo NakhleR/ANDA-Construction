f4dbd806212c5e9c69db6d84a6e93df52ce8b5fc
NakhleR <nakhlerizk@gmail.com>
2025-01-07 18:27:27 +0100

    Added Bento grid to portfolio

--- src/components/Porfolio.jsx
+++ src/components/Porfolio.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
-import { projectImg1 } from '../utils';
+import { ImageList, ImageListItem, ImageListItemBar } from '@mui/material';
+import { itemData } from '../constants';
 
 const Portfolio = () => {
     return (
         <section className="min-h-screen bg-accent p-12">
             <div className="text-center mb-16">
                 <h1 className="text-5xl font-fraunces font-bold text-accent2 mb-4">My Portfolio</h1>
-                <p className="text-lg font-avenir text-accent2">Welcome to my portfolio. Explore my projects to learn more about what I do.</p>
+                <p className="text-lg font-avenir mb-11 text-accent2">Welcome to my portfolio. Explore my projects to learn more about what I do.</p>
+                <ImageList variant="masonry" cols={3} gap={15}>
+                    {itemData.map((item) => (
+                        <ImageListItem key={item.img}>
+                            <img
+                                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                src={`${item.img}?w=248&fit=crop&auto=format`}
+                                alt={item.title}
+                                loading="lazy"
+                            />
+                            <div className='flex my-11 flex-col items-start justify-start'>
+                                <h1 className='font-fraunces font-semibold text-xl'>Project Title</h1>
+                                <p className='font-avenir mt-4 text-md text-left'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate laudantium a quae at, consectetur nisi soluta esse sed sint expedita necessitatibus distinctio iste id numquam ducimus corrupti ipsam. Possimus, voluptatibus.</p>
+                            </div>
+                            <hr className="border-t-2 border-accent2 my-11" />
+                        </ImageListItem>
+                    ))}
+                </ImageList>
             </div>
-        </section>
+        </section >
     );
 }
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,36 @@
 export const navLinks = ["Portfolio", "Blog", "Contact"];
+
+export const itemData = [
+    {
+        img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
+        title: 'Kitchen',
+        author: 'Christian Mackie',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
+        title: 'Blinds',
+        author: 'Darren Richardson',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
+        title: 'Coffee table',
+        author: 'Hutomo Abrianto',
+    },
+
+    {
+        img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+        title: 'Storage',
+        author: 'Douglas Sheppard',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
+        title: 'Candle',
+        author: 'Fi Bell',
+    },
+
+    {
+        img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
+        title: 'Doors',
+        author: 'Philipp Berndt',
+    },
+];
