c6cdf324b55fd44d40f902fb4f2e4accb818cdec
NakhleR <nakhlerizk@gmail.com>
2025-01-22 00:10:21 +0100

    Minor fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
     };
 
     return (
-        <section id='contact' className="min-h-screen px-6 py-12 sm:px-14 md:px-24 w-full bg-accent">
+        <section id="contact" className="min-h-screen px-4 sm:px-6 py-12 md:px-24 w-full bg-accent">
             <div>
                 {/* Heading */}
                 <h2 className="text-accent2 font-fraunces font-bold text-2xl sm:text-3xl md:text-[35px] text-center sm:text-left">
@@ -22,36 +22,36 @@ const Contact = () => {
                         <form ref={form} onSubmit={sendEmail}>
                             {/* Input Fields */}
                             <div className="flex flex-col gap-4">
-                                <div className="flex gap-4">
+                                <div className="flex flex-col sm:flex-row gap-4">
                                     <input
                                         type="text"
                                         placeholder="Nom"
                                         name="user_nom"
                                         required
-                                        className="flex-1 bg-accent border focus:rounded-none placeholder-accent2 focus:border-accent2 focus:outline-accent2 text-accent2 font-avenir border-accent2 p-3"
+                                        className="flex-1 w-full bg-accent border focus:rounded-none placeholder-accent2 focus:border-accent2 focus:outline-accent2 text-accent2 font-avenir border-accent2 p-3"
                                     />
                                     <input
                                         type="text"
                                         placeholder="Prénom"
                                         name="user_prenom"
                                         required
-                                        className="flex-1 bg-accent border focus:rounded-none placeholder-accent2 focus:border-accent2 focus:outline-accent2 text-accent2 font-avenir border-accent2 p-3"
+                                        className="flex-1 w-full bg-accent border focus:rounded-none placeholder-accent2 focus:border-accent2 focus:outline-accent2 text-accent2 font-avenir border-accent2 p-3"
                                     />
                                 </div>
-                                <div className="flex gap-4">
+                                <div className="flex flex-col sm:flex-row gap-4">
                                     <input
                                         type="text"
                                         placeholder="Adresse"
                                         name="user_adresse"
                                         required
-                                        className="flex-1 bg-accent border focus:rounded-none placeholder-accent2 focus:border-accent2 focus:outline-accent2 text-accent2 font-avenir border-accent2 p-3"
+                                        className="flex-1 w-full bg-accent border focus:rounded-none placeholder-accent2 focus:border-accent2 focus:outline-accent2 text-accent2 font-avenir border-accent2 p-3"
                                     />
                                     <input
                                         type="text"
                                         placeholder="Ville"
                                         name="user_ville"
                                         required
-                                        className="flex-1 bg-accent border focus:rounded-none placeholder-accent2 focus:border-accent2 focus:outline-accent2 text-accent2 font-avenir border-accent2 p-3"
+                                        className="flex-1 w-full bg-accent border focus:rounded-none placeholder-accent2 focus:border-accent2 focus:outline-accent2 text-accent2 font-avenir border-accent2 p-3"
                                     />
                                 </div>
                                 <input
@@ -59,20 +59,20 @@ const Contact = () => {
                                     placeholder="Email"
                                     name="user_email"
                                     required
-                                    className="bg-accent border focus:rounded-none placeholder-accent2 focus:border-accent2 focus:outline-accent2 text-accent2 font-avenir border-accent2 p-3"
+                                    className="w-full bg-accent border focus:rounded-none placeholder-accent2 focus:border-accent2 focus:outline-accent2 text-accent2 font-avenir border-accent2 p-3"
                                 />
                                 <textarea
                                     type="text"
                                     name="project_description"
                                     placeholder="Petite description de votre projet (dites nous plus sur votre projet)"
                                     required
-                                    className="bg-accent border focus:rounded-none placeholder-accent2 focus:border-accent2 focus:outline-accent2 text-accent2 font-avenir border-accent2 p-3 w-full h-40"
+                                    className="w-full bg-accent border focus:rounded-none placeholder-accent2 focus:border-accent2 focus:outline-accent2 text-accent2 font-avenir border-accent2 p-3 h-40"
                                 ></textarea>
                             </div>
                             {/* Submit Button */}
                             <button
                                 type="submit"
-                                className="border mt-4 border-accent2 font-avenir px-4 sm:px-6 py-2 text-sm sm:text-base text-accent2 hover:bg-accent3 transition-all"
+                                className="border mt-4 border-accent2 font-avenir px-4 sm:px-6 py-2 text-sm sm:text-base text-accent2 hover:bg-accent3 transition-all w-auto sm:w-auto"
                             >
                                 Send
                             </button>
