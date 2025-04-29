a709e52f83934783f3893217b575472052f99974
NakhleR <nakhler2k2@gmail.com>
2025-04-29 17:44:13 +0200

    Added config for rerouting

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// Get repository name to set base path for GitHub Pages
-// If local development or not GitHub pages, this will be '/'
-const getRepositoryName = (): string => {
-  // Check if GitHub pages environment
-  if (process.env.GITHUB_REPOSITORY) {
-    const [, repo] = process.env.GITHUB_REPOSITORY.split('/')
-    return `/${repo}/`
-  }
-  return '/'
-}
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -27,5 +16,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: getRepositoryName(),
 }));
